b400a62bd6c21354930a9f88310379dffcb1bcf3
Jordi Mas <jmas@softcatala.org>
2022-03-29 14:06:23 +0200

    Fix

--- catalan/create_shared_task_data_from_text_file.py
+++ catalan/create_shared_task_data_from_text_file.py
@@ -10,11 +10,11 @@ PUNCT_MAP = {'!': '.', ';': '.'}
 
 WORD_SPLIT = '(\.|,|\?|-|:|!|;| )'
 
-def to_tsv(text_file: str, outpath: str):
+def to_tsv(lines, filename: str):
     label_counts = Counter()
-
-    with open(text_file, "r") as textfile_fh, open("train.txt", 'w', encoding='utf8') as output_fh:
-        for line in textfile_fh.readlines():
+    
+    with open(filename, 'w', encoding='utf8') as output_fh:
+        for line in lines:
             for tok in re.split(WORD_SPLIT, line):
 
                 if len(tok.strip()) == 0:
@@ -35,13 +35,28 @@ def to_tsv(text_file: str, outpath: str):
 #                print(s)
                 output_fh.write(s+ "\n")
 
-    print()
+    print(filename)
     for label, cnt in label_counts.most_common():
         print(label, cnt)
 
 
+def dev_test_train(text_file: str, outpath: str):
+
+    with open(text_file, "r") as textfile_fh:
+        lines = textfile_fh.readlines()
+        random.shuffle(lines)
+
+        train_lines, test_lines = train_test_split(lines, test_size=.16, random_state=RAND_SEED)
+        train_lines, dev_lines = train_test_split(train_lines, test_size=.2, random_state=RAND_SEED)
+
+        to_tsv(train_lines, "ca/train/train.tsv")
+        to_tsv(dev_lines, "ca/train/dev.tsv")
+        to_tsv(test_lines, "ca/train/test.tsv")
+
 if __name__ == '__main__':
-    to_tsv('catalan.txt',  outpath='data/sepp_nlg_2021_data/')
+    dev_test_train('catalan.txt',  outpath='data/sepp_nlg_2021_data/')
+
+
 #    create_europarl_data('data/de.zip', lang='de', outdir='data/sepp_nlg_2021_data/')
 #    create_europarl_data('data/fr.zip', lang='fr', outdir='data/sepp_nlg_2021_data/')
 #    create_europarl_data('data/it.zip', lang='it', outdir='data/sepp_nlg_2021_data/')

2d8b0dd57dbcedcd998a22c851315f312a2ce39f
Jordi Mas <jmas@softcatala.org>
2022-04-04 09:13:40 +0200

    scpi tokenizer

--- catalan/create_shared_task_data_from_text_file.py
+++ catalan/create_shared_task_data_from_text_file.py
@@ -2,6 +2,8 @@ import random
 from collections import Counter
 from sklearn.model_selection import train_test_split
 import re
+import os
+import pathlib
 
 RAND_SEED = 42
 random.seed(RAND_SEED)
@@ -25,15 +27,29 @@ def tokenize_sentence_into_words(sentence):
 
     return words
 
+def _create_directories(filename):
+
+    directory = os.path.dirname(os.path.abspath(filename))
+    pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
+
+
 def to_tsv(lines, filename: str):
+
+    _create_directories(filename)
     label_counts = Counter()
     
     with open(filename, 'w', encoding='utf8') as output_fh:
         prev = None
+        cnt_lines = 0
         for line in lines:
+            cnt_lines += 1
+                
             line = line.lower()
             if '\"' in line:
                 continue
+
+            if cnt_lines % 1000000 == 0:
+                print("Processed {lines} lines")
 
             for tok in tokenize_sentence_into_words(line):
                 tok = tok.strip()
@@ -77,13 +93,17 @@ def dev_test_train(text_file: str, outpath: str):
         train_lines, test_lines = train_test_split(lines, test_size=.16, random_state=RAND_SEED)
         train_lines, dev_lines = train_test_split(train_lines, test_size=.2, random_state=RAND_SEED)
 
-        to_tsv(train_lines, "sepp_nlg_2021_data/ca/train/train.tsv")
-        to_tsv(dev_lines, "sepp_nlg_2021_data/ca/dev/dev.tsv")
-        to_tsv(test_lines, "sepp_nlg_2021_data/ca/test/test.tsv")
+        to_tsv(train_lines, f"{outpath}/ca/train/train.tsv")
+        to_tsv(dev_lines, f"{outpath}/ca/dev/dev.tsv")
+        to_tsv(test_lines, f"{outpath}/ca/test/test.tsv")
 
     print(f"Wrote zip into {outpath} using {text_file} as input corpus")
 
 if __name__ == '__main__':
+    input_corpus = 'europarl.en-ca.ca'
+    outpath = 'sepp_nlg_2021_data/'
+    zipfile = 'data-ca.zip'
 
-    dev_test_train('europarl.en-ca.ca',  outpath='data/sepp_nlg_2021_data/')
-
+    dev_test_train(input_corpus,  outpath=outpath)
+    os.system(f"zip -r {zipfile} {outpath}")
+    

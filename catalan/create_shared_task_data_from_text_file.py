8e153a9816ca20cef6becfb6b1c95fb4ad6ebd40
Jordi Mas <jmas@softcatala.org>
2022-04-04 09:23:25 +0200

    Fix

--- catalan/create_shared_task_data_from_text_file.py
+++ catalan/create_shared_task_data_from_text_file.py
@@ -49,7 +49,7 @@ def to_tsv(lines, filename: str):
                 continue
 
             if cnt_lines % 10000 == 0:
-                print("Processed {lines} lines")
+                print(f"Processed {lines} lines")
 
             for tok in tokenize_sentence_into_words(line):
                 tok = tok.strip()

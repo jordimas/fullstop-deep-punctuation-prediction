2d49aac168fe5b8e0bb35d7c2b6f3faba6fa0d07
Jordi Mas <jmas@softcatala.org>
2022-04-06 10:38:17 +0200

    Fixes

--- flores-test.sh
+++ flores-test.sh
@@ -1,6 +1,4 @@
-sed 's/,//g' flores101_cat.txt  > flores101_cat_no_comas.txt
 head -n 300 flores101_cat.txt > flores101_cat_small.txt
-head -n 300 flores101_cat_no_comas.txt > flores101_cat_no-comas_small.txt
 python3 inference.py
 #diff -u flores101_cat_small.txt output.txt > small.txt
-python3 diff.py
+#python3 diff.py
--- inference.py
+++ inference.py
@@ -1,39 +1,70 @@
 # -*- coding: utf-8 -*-
 
-import sys
 import datetime
-
-import logging
 from punctuationmodel import PunctuationModel
 
-if __name__ == "__main__":
 
-    start_time = datetime.datetime.now()
+def do_inference(ref_lines, lines, output, file_type):
 
-    with open('flores101_cat_no-comas_small.txt', 'r') as fp:
-        test_sample = fp.readlines()
-
-    with open('output.txt', 'w') as ot:
+    with open(output, 'w') as ot:
 
         model = PunctuationModel()
 
         cnt = 0
-        for line in test_sample:
+        equal = 0
+        diff = 0
+        for idx in range(0, len(lines)):
+            line = lines[idx].strip()
+            ref_line = ref_lines[idx].strip()
+        
             line = line.strip()
             if len(line) == 0:
                 continue
 
 #            print(f"source: '{line}'")
-            punctuated = model.restore_punctuation(line)
+            line = model.restore_punctuation(line)
 
-            ot.write(punctuated + "\n")
+            ot.write(line + "\n")
             cnt += 1
 
-            if cnt % 50 == 0:
-                print(cnt) 
-            
- #           print(f"result: '{punctuated}'")
- #           print("--")
+            if cnt % 100 == 0:
+                print(cnt)
+
+#            print(f"{line == ref_line} - '{ref_line}' - '{line}'")
+            if line == ref_line:
+                equal += 1
+            else:
+                diff += 1
+
+    pequal = equal * 100 / (diff+equal)
+    print(f"Sentences: for {file_type} equal {equal}  ({pequal:.2f}%), diff {diff}")
+
+
+if __name__ == "__main__":
+
+    start_time = datetime.datetime.now()
+
+    with open('flores101_cat_small.txt', 'r') as fp:
+        lines = fp.readlines()
+    
+    with open('flores101_cat_no-comas.txt', 'w') as fp:
+        processed_lines = []
+        for line in lines:
+            line = line.replace(",", "")
+            fp.write(line)
+            processed_lines.append(line)
+
+        do_inference(lines, processed_lines, "output-no-commas.txt", "no commas")
+
+    with open('flores101_cat_no-comas-no-dots.txt', 'w') as fp:
+        processed_lines = []
+        for line in lines:
+            line = line.replace(",", "")
+            line = line.replace(".", "")
+            fp.write(line)
+            processed_lines.append(line)
+
+        do_inference(lines, processed_lines, "output-flores101_cat_no-comas-no-dots.txt", "no commas and no dots")
 
     s = 'Time used: {0}'.format(datetime.datetime.now() - start_time)
     print(s)

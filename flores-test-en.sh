fb30f46e83ddb992ef9710db33d00069c8fdd13d
Jordi Mas <jmas@softcatala.org>
2022-04-06 14:47:34 +0200

    Fix

--- flores-test-en.sh
+++ flores-test-en.sh
@@ -1,3 +1,3 @@
 #sed 's/,//g' flores101_eng.txt  > flores101_eng_no_comas.txt
 head -n 300 flores101_eng.txt > flores101_eng_small.txt
-python3 inference.py
+python3 inference.py flores101_eng_small.txt
